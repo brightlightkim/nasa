ec6fbadf2cf06f7b96a4a633ffea7c33f5031b5d
brightlightkim <taeyangk95@naver.com>
2021-10-16 21:14:25 +0900

    remove some unnecessary comments

--- script.js
+++ script.js
@@ -33,15 +33,6 @@ document.getElementById("dateSubmit").addEventListener("click", function (event)
             .then(function (response) {
                 return response.json();
             }).then(function (json) {
-                /*
-                let firstImage = '<img src="' + json[0].hdurl + '"/>';
-                document.getElementById("main_picture").innerHTML = firstImage;
-                
-                let explanation = "";
-                explanation += '<h4>' + json[0].title + '</h4><br>';
-                explanation += '<p>' + json[0].explanation + '</p>';
-                document.getElementById("main_picture_explanation").innerHTML = explanation;
-                */
                 let images = "";
                 for (let i = 1; i < json.length; i++){
                     images += '<img src="' + json[i].hdurl + '"/>';
