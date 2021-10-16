158994ac6b7b16e46306d741800a7d58779bf524
brightlightkim <taeyangk95@naver.com>
2021-10-16 21:08:59 +0900

    changed

--- script.js
+++ script.js
@@ -28,6 +28,35 @@ document.getElementById("dateSubmit").addEventListener("click", function (event)
     }
 });
 
+document.getElementById("dateSubmit").addEventListener("click", function (event) {
+    event.preventDefault();
+    const value = document.getElementById("dateInput").value;
+    if (value === "")
+        return;
+    else {
+        const url = "https://api.nasa.gov/planetary/apod?api_key=y5pneC0smGlo2LueKD3szEc4gM6Qh0sgOlh96wFx&count=7"
+        fetch(url)
+            .then(function (response) {
+                return response.json();
+            }).then(function (json) {
+                let firstImage = '<img src="' + json[0].hdurl + '"/>';
+                document.getElementById("main_picture").innerHTML = firstImage;
+                
+                let explanation = "";
+                explanation += '<h4>' + json[0].title + '</h4><br>';
+                explanation += '<p>' + json[0].explanation + '</p>';
+                document.getElementById("main_picture_explanation").innerHTML = explanation;
+
+                let images = "";
+                for (let i = 1; i < json.length; i++){
+                    images += '<img src="' + json[i].hdurl + '"/>';
+                }
+                document.getElementById("nasa_picture").innerHTML = images;
+            });
+
+    }
+});
+
 
 
 /*
